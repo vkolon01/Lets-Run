5ef3bc06a8c8dfc8a2477631a0d6f739b3c7e43d
vkolon01 <vkolon01@vmail.bbk.ac.uk>
2018-01-10 23:35:21 +0000

    www update

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,5 +1,5 @@
 export default {
-  SEVER_URL: "http://192.168.0.3:4000",
+  SEVER_URL: "http://192.168.0.3:8080",
   BAD_AUTH: "BAD_AUTH",
   GOOGOLE_API_KEY: "AIzaSyBuKY6REalbK20B1J8JjCutMn3p14oy02c"
 }
