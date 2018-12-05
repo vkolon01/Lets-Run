64df930b692941b2b5a5c6a558c47df708c44f8e
sejimf <kolodkomaxim@gmail.com>
2018-12-05 22:48:49 +0200

    Update

--- client/src/constants/constants.js
+++
@@ -1,5 +0,0 @@
-export default {
-  SEVER_URL: "http://localhost:8080/api",
-  BAD_AUTH: "BAD_AUTH",
-  GOOGOLE_API_KEY: "AIzaSyBuKY6REalbK20B1J8JjCutMn3p14oy02c"
-}
--- client/src/services/History.js
+++
@@ -1,9 +0,0 @@
-import createHistory from 'history/createHashHistory';
-
-const history = createHistory();
-
-history.listen((location,action) => {
-
-})
-
-export default history;
