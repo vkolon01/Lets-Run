9d54c66533c410ad825dbb95f27714b52a85da4e
K.Maksim <kolodkomaxim@gmail.com>
2019-02-08 22:52:11 +0200

    deleted unused files

--- Lets-Run-client/letsRun/src/app/forum/forum.module.ts
+++ Lets-Run-client/letsRun/src/app/forum/forum.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { RouterModule } from "@angular/router";
 import { ReactiveFormsModule } from "@angular/forms";
 import { AngularMaterialModule } from "../angular-material.module";
 import { GeneralCategoryComponent } from "./general-category/general-category.component";
@@ -8,8 +7,6 @@ import { UsersCategoryComponent } from "./users-category/users-category.componen
 import { MainListComponent } from "./main-list/main-list.component";
 import { ForumRouterModule } from "./forum-router.module";
 import { ForumItemComponent } from "./forum-item/forum-item.component";
-import { PostItemComponent } from "./forum-item/post-list-with-desc/post-item/post-item.component";
-import { PostCommentComponent } from "./forum-item/post-list-with-desc/post-item/post-detail/post-comment/post-comment.component";
 
 @NgModule({
     declarations: [MainListComponent,
